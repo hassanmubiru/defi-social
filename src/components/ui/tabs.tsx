5dc1808b2cfa0b238dbd1ededfe7d4fbb153c2e1
hassanmubiru <hassanmubiru7@gmail.com>
2025-08-26 06:55:10 +0300

    new file:   src/components/ui/badge.tsx 	modified:   src/components/ui/tabs.tsx

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,8 @@
+<Tabs defaultValue="tab1">
+  <TabsList>
+    <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+    <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+  </TabsList>
+  <TabsContent value="tab1">Content 1</TabsContent>
+  <TabsContent value="tab2">Content 2</TabsContent>
+</Tabs>
